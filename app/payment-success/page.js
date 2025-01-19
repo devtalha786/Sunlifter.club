7b8cb325f3eec487932feb9c88cad363c6587a7e
Talha Zia <ziatalhazia@gmail.com>
2025-01-19 22:51:39 +0500

    update page for build

--- app/payment-success/page.js
+++ app/payment-success/page.js
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { Suspense } from 'react';
 import PaymentSuccessComp from './PaymentSuccessComp';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -8,18 +9,20 @@ const page = () => {
 		<>
 			<Navbar />
 			<div className="relative bg-[url('/assets/profile-mobile.png')] sm:bg-[url('/assets/profile.png')] bg-cover bg-no-repeat">
-				<div className='bg-black/10 inset-0 w-full h-full absolute'></div>
-				<div className='px-4 lg:px-6 xl:px-10 relative z-40'>
-					<div className='max-w-[1600px] mx-auto'>
-						<div className='max-w-[512px] pt-[175px] pb-[72px] sm:py-[152px]'>
-							<h1 className='text-white text-[32px] sm:text-[48px] leading-[37px] sm:leading-[56px] font-workSpace font-bold tracking-[-0.02em] mb-2.5'>
+				<div className="bg-black/10 inset-0 w-full h-full absolute"></div>
+				<div className="px-4 lg:px-6 xl:px-10 relative z-40">
+					<div className="max-w-[1600px] mx-auto">
+						<div className="max-w-[512px] pt-[175px] pb-[72px] sm:py-[152px]">
+							<h1 className="text-white text-[32px] sm:text-[48px] leading-[37px] sm:leading-[56px] font-workSpace font-bold tracking-[-0.02em] mb-2.5">
 								Payment
 							</h1>
 						</div>
 					</div>
 				</div>
 			</div>
-			<PaymentSuccessComp />
+			<Suspense fallback={<div>Loading payment details...</div>}>
+				<PaymentSuccessComp />
+			</Suspense>
 			<Footer />
 		</>
 	);
